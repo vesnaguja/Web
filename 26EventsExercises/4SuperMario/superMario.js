090aee912a56b7d75f823cfd16f70928b4a7b257
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2021-06-11 23:30:13 +0200

    Completed events exercises

---
+++ 26EventsExercises/4SuperMario/superMario.js
@@ -0,0 +1,55 @@
+var body = document.querySelector('body');
+var background = document.querySelector('.container')
+var mario = document.querySelector('#mario');
+var marioRunning = false;
+var i = 0;
+//var jumpIndex = 0;
+
+body.addEventListener('keydown', keyDownHandler);
+body.addEventListener('keyup', keyUpHandler);
+
+function keyDownHandler(event) {
+  if (event.code === 'ArrowRight') {
+    if (marioRunning === false) {
+      marioRunning = true;
+      mario.src = './images/mario_running.gif';
+    }
+
+    background.style.backgroundPosition = i + 'px 0px';
+
+    i -= 10;
+  }
+
+  if (event.code === 'ArrowLeft') {
+    if (marioRunning === false) {
+      marioRunning = true;
+      mario.src = './images/mario_running_left.gif';
+    }
+    
+    background.style.backgroundPosition = i + 'px 0px';
+
+    i += 10;
+  }
+
+  if (event.code === 'ArrowUp') {
+    // mario jump
+
+    // var top = parseInt(mario.style.top);
+    // if(top > 30) {
+    //   mario.style.top = top - jumpIndex + '%';
+    //   jumpIndex += 5;
+    // }
+
+    mario.style.top = 30 + '%';
+  }
+
+ 
+}
+
+function keyUpHandler(event) {
+  marioRunning = false;
+  mario.src = './images/mario.png';
+  mario.style.top = 58 + '%';
+
+  //jumpIndex = 0;
+}
