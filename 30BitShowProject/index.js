bc036acf8f075cfa686decfe570814edd0b5d79a
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2021-07-07 20:58:34 +0200

    Finished 2nd page layout

--- 30BitShowProject/index.js
+++ 30BitShowProject/index.js
@@ -9,6 +9,8 @@ request.done(function (response) {
   }).slice(0, 50);
   //console.log(top50);
 
+  $('#rowResult').html('');
+
   top50.forEach(function (item) {
     var poster = item.image.medium;
     var title = item.name;
@@ -16,10 +18,10 @@ request.done(function (response) {
 
     $('#rowResult').append(`
     <div class="col-4 img-and-title" data-show="${item.id}">
-    <div class="border mb-4">
-    <img src="${poster}" class="showImg" data-show="${item.id}">
-    <p class="showTitle p-3 fw-bold" data-show="${item.id}">${title}</p>
-    </div>
+      <div class="border mb-4">
+        <img src="${poster}" class="showImg" data-show="${item.id}">
+        <p class="showTitle p-3 fw-bold" data-show="${item.id}">${title}</p>
+      </div>
     </div>`)
   })
 })
@@ -33,21 +35,18 @@ $(document).on('click', '.img-and-title', function (e) {
 })
 
 
-$('#search-show').on('keyup', function (e) {
+// $('#search-show').on('keydown', function (e) {  
 
-  if (e.key === 'Enter') {
-    console.log('nesto');
-    // var inputSearch = e.target.value;
-    // console.log(e.target.value);
+//   if (e.key === 'Enter') {
+//     e.preventDefault();
+//     // console.log('nesto');
+//     var inputSearch = e.target.value;
+//     e.target.value = '';
 
-    // //window.localStorage.clear();
-    // window.localStorage.setItem('searchedShow', inputSearch)
-    // window.open('./showInfo.html', "_blank");
+//     //window.localStorage.clear();
+//     window.localStorage.setItem('searchedShow', inputSearch)
+//     window.open('./showInfo.html', "_blank");
 
+//   }
 
-
-  }
-  //e.target.value = '';
-
-
-})
+// })
--- 30BitShowProject/showInfo.js
+++ 30BitShowProject/showInfo.js
@@ -1,9 +1,43 @@
-const show = window.localStorage.getItem('showId');
-const showNameElement = document.querySelector('#show');
+var show = window.localStorage.getItem('showId');
+console.log(show);
 
-showNameElement.textContent = user;
+// var show = window.localStorage.getItem('searchedShow');
 
 var request = $.ajax({
-  url: 'http://api.tvmaze.com/search/shows?q=' + inputSearch
- 
+  url: 'http://api.tvmaze.com/shows/' + show
+})
+
+
+request.done(function (response) {
+  // console.log(show);
+
+  $('.showTitle').append(`<h2 class="title p-3">${response.name}</h2>`)
+  $('.posterImg').append(`<img src="${response.image.medium}" class="w-100">`)
+  $('.showDetails').append(`<p>${response.summary}</p>`)
+})
+
+// request for seasons
+
+var request = $.ajax({
+  url: ' http://api.tvmaze.com/shows/' + show + '/seasons'
+})
+
+request.done(function (response) {
+  $('.numOfSeasons').append(`(${response.length})`)
+  // console.log(response.length);
+  response.forEach(function (season) {
+    $('.seasons').append(`<li>${season.premiereDate} - ${season.endDate}</li>`)
+  })
+})
+
+//request for cast
+
+var request = $.ajax({
+  url: 'http://api.tvmaze.com/shows/' + show + '/cast'
+})
+
+request.done(function (response) {
+  response.forEach(function (cast) {
+    $('.cast').append(`<li>${cast.person.name}</li>`)
+  })
 })
